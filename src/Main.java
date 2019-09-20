a48eb50d86b7eda70175902209c325276158d94f
athimineur <athimineur@aol.com>
2019-09-20 11:13:19 -0400

    updated main file with a println statement

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		Cat cat = new Cat("Callie", 1, "Tortoiseshell", "Female");
 		System.out.println(cat);
+		System.out.println("hello");
 	}
 }
